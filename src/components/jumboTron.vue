9fefa46752ece8280e47df6331350c1c7615f6ff
Simone Saputo <simonesaputo0@gmail.com>
2022-04-11 18:33:24 +0200

    first-commit

---
+++ src/components/jumboTron.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="vue-cli-first\src\assets\evidenza-1-1000x600.jpg" alt="Paesaggio">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'JumboTron',
+}
+</script>
+
+
+<style scoped>
+    img {
+        width: 80vw;
+        height: 80vh;
+    }
+</style>
